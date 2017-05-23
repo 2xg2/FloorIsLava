c379498257236747cfb0b236092f0768254ed472
2xg2 <growupwhatfor@gmail.com>
2017-05-23 23:41:00 +0300

    ball improvements

---
+++ Assets/Scripts/BallCenter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCenter : MonoBehaviour
+{
+    void Update()
+    {
+        transform.forward = Camera.main.transform.forward;
+    }
+}
